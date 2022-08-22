bf9af8722fe20356513532ae0ec8402a9549d507
Ruslana Solovova <ruslanasolovova@gmail.com>
2022-08-22 11:52:40 -0700

    first commit

---
+++ script.py
@@ -0,0 +1,27 @@
+from ast import Return
+from cgi import print_arguments
+
+
+calculation_of_units = 24
+num_of_unit = "hours"
+
+def days_to_units(num_of_days):
+        return f"{num_of_days} days are {num_of_days * calculation_of_units} {num_of_unit}"
+
+def validate_and_execute():
+    try: 
+        user_input_number = int(user_input)
+        if user_input_number > 0:
+            calculated_value = days_to_units(user_input_number)
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("you enetered a 0, enter positive number")
+        else:
+            print("you entered negative number, please enter positive number")
+            
+    except ValueError :
+        print("your input in not valid input")
+    
+user_input = input("Enter number of days to convert it to hours\n")
+validate_and_execute()
+
